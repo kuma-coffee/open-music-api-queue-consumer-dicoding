818ac16611b9d3c0b6107453b59928862ddd0b8e
kuma-coffee <daniel099d@gmail.com>
2023-11-29 14:29:40 +0700

    minor fix

--- src/SongsService.js
+++ src/SongsService.js
@@ -6,7 +6,16 @@ class SongsService {
   }
 
   async getSongsOnPlaylist(playlistId, userId) {
-    const query = {
+    const playlistQuery = {
+      text: `SELECT p.id, p.name FROM playlists AS p 
+      WHERE p.id = $1 AND p.owner = $2`,
+      values: [playlistId, userId],
+    };
+    const playlistResult = await this._pool.query(playlistQuery);
+
+    const playlistData = playlistResult.rows[0];
+
+    const songsQuery = {
       text: `SELECT s.id, s.title, s.performer FROM songs AS s
       LEFT JOIN playlist_song_activities AS ps ON ps.song_id = s.id
       WHERE ps.playlist_id = $1 AND ps.user_id = $2
@@ -14,9 +23,16 @@ class SongsService {
       values: [playlistId, userId],
     };
 
-    const result = await this._pool.query(query);
+    const songsResult = await this._pool.query(songsQuery);
 
-    return result.rows;
+    const songsData = songsResult.rows;
+
+    return {
+      playlist: {
+        ...playlistData,
+        songs: [...songsData],
+      },
+    };
   }
 }
 
